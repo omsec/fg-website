66abb06a66c15c47f91ab2631fff6e7952c0b2b8
omsec <roger@eierwerfer.com>
2021-08-17 09:55:56 +0200

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,16 @@
-export const environment = {
+// interface mnauell gebaut für typisierung (ermöglicht envPipe)
+export interface AppEnvironment {
+  production: boolean;
+  apiUrl: string;
+  imgUrl: string;
+  releaseDate: Date;
+}
+
+const urlBase = 'https://192.168.1.10:3000';
+
+export const environment: AppEnvironment = {
   production: true,
-  apiUrl: 'http://192.168.1.14:3000'
+  apiUrl: urlBase,
+  imgUrl: urlBase + '/images',
+  releaseDate: new Date('2020-01-01')
 };
